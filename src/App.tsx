88e220fd0ab04a98fcd210b2d52425c85025cbc7
geraldojalves <junior.alves.2007@gmail.com>
2024-02-22 23:05:09 -0300

    chore: create default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export default function App() {
   return (
     <div>
-      <h1>Coffee Delivery</h1>
+      <ThemeProvider theme={defaultTheme}>
+        <h1>Coffee Delivery</h1>
+      </ThemeProvider>
     </div>
   )
 }
