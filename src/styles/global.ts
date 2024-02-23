67cf7f5b87b7b04f1800ae8a044610ef86e04de7
geraldojalves <junior.alves.2007@gmail.com>
2024-02-22 23:21:03 -0300

    chore: add Roboto and Baloo 2

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,9 +5,16 @@ export const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  line-height: 1.3;
+  font-family: Roboto, sans-serif;
 }
+
 body {
   background: #333;
   color: #FFF;
+}
+
+:focus {
+  outline: 0;
 }
 `
